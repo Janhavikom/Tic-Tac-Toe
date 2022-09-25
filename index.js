04ff7d1e548604bfa2ce8e3e136fa8c3672aa077
Janhavikom <84494006+Janhavikom@users.noreply.github.com>
2022-09-26 00:33:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+let music = new Audio("music.mp3")
+let turnmusic = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let gameend = false;
+let turn = "X";
+const changeTurn = () =>{
+    return turn==="X"?"0":"X";
+}
+
+const checkWin = () =>{
+    let boxtext = document.getElementsByClassName('boxtext');
+let wins = [
+    [0,1,2,5,5,0],
+    [3,4,5,5,5,15],
+    [6,7,8,5,25,0],
+    [0,3,6,-5,15,90],
+    [1,4,7,5,15,90],
+    [2,5,8,15,15,90],
+    [0,4,8,5,15,45],
+    [2,4,6,5,15,135],
+]
+wins.forEach(e =>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
+document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+gameend = true;
+music.play();
+document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+document.querySelector(".line").style.width = "20vw"
+document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+    }
+    
+})
+}
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click',()=>{
+        if (boxtext.innerText ===''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            turnmusic.play();
+            checkWin();
+            if (!gameend)
+            {
+            document.getElementsByClassName("info")[0].innerText = "Turn for "+turn
+            }
+        }
+    })
+})
+
+// let reset = document.getElementById("reset")
+reset.addEventListener('click',()=>{
+    let boxes1 = document.querySelectorAll(".boxtext");
+    Array.from(boxes1).forEach(element => {
+element.innerText = '';
+    });
+    turn = 'X';
+    gameend = false;
+    music.pause();
+    document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    document.querySelector(".line").style.width = "0"
+})
